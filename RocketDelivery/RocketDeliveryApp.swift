1092980cfb2ac6c1b568ea5ca7ff86e93b714777
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-03-11 20:50:04 -0300

    feat: start app

---
+++ RocketDelivery/RocketDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketDeliveryApp.swift
+//  RocketDelivery
+//
+//  Created by Rhuan Carvalho on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct RocketDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
