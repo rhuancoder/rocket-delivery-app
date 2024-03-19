7991427af960d7e26ba3b245aebfa358d7545293
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-03-19 20:10:31 -0300

    feat: add images and mocks

--- RocketDelivery/GridView/OrderTypeGridView.swift
+++ RocketDelivery/GridView/OrderTypeGridView.swift
@@ -9,15 +9,13 @@ import SwiftUI
 
 struct OrderTypeGridView: View {
     
-    let orders = ["Restaurants", "Drugstore", "Discounts", "Gourmet", "Markets", "Pet", "Drinks"]
-    
     var body: some View {
         LazyHGrid(rows: [
             GridItem(.fixed(100)),
             GridItem(.fixed(100))
         ]) {
-            ForEach(orders, id: \.self) { orderItem in
-                Text(orderItem)
+            ForEach(ordersMock) { orderItem in
+                Text(orderItem.name)
             }
         }
     }
