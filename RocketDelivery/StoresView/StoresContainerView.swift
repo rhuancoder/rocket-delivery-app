1b4033ebdada592468af0c3786a87ae1ca2b5490
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-03-29 15:55:40 -0300

    feat: add stores container view

---
+++ RocketDelivery/StoresView/StoresContainerView.swift
@@ -0,0 +1,24 @@
+//
+//  StoresContainerView.swift
+//  RocketDelivery
+//
+//  Created by Rhuan Carvalho on 29/03/24.
+//
+
+import SwiftUI
+
+struct StoresContainerView: View {
+    
+    let title = "Stores"
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.headline)
+        }
+    }
+}
+
+#Preview {
+    StoresContainerView()
+}
