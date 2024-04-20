46fe438e22cc886bb55316475b85a4e5e82d9089
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-04-20 16:45:01 -0300

    feat: add elements in product detail view

--- RocketDelivery/ProductView/ProductDetailView.swift
+++ RocketDelivery/ProductView/ProductDetailView.swift
@@ -12,7 +12,48 @@ struct ProductDetailView: View {
     let product: ProductType
     
     var body: some View {
-        Text(product.name)
+        VStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(radius: 20)
+                
+                Text(product.name)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .padding(.horizontal)
+                
+                Text(product.description)
+                    .padding(.horizontal)
+                
+                Text(product.formattedPrice)
+                    .font(.title3)
+                    .bold()
+                    .padding(.horizontal)
+            }
+        }
+        
+        Spacer()
+        
+        Button {
+            print("The button has been pressed")
+        } label: {
+            HStack {
+                Image(systemName: "cart")
+                
+                Text("Add to cart")
+            }
+            .padding(.horizontal, 32)
+            .padding(.vertical, 16)
+            .font(.title3)
+            .bold()
+            .background(.colorRed)
+            .foregroundColor(.white)
+            .cornerRadius(32)
+            .shadow(color: .colorRedDark.opacity(0.5), radius: 10, x: 6, y: 8)
+        }
+
     }
 }
 
