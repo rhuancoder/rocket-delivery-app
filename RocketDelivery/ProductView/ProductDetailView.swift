5c6e6f01e999a1d1142e509dcc59104c70ebd9a0
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-04-20 17:00:24 -0300

    feat: add quantity in product detail view

--- RocketDelivery/ProductView/ProductDetailView.swift
+++ RocketDelivery/ProductView/ProductDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ProductDetailView: View {
     
     let product: ProductType
+    @State private var productQuantity = 1
     
     var body: some View {
         VStack {
@@ -31,6 +32,40 @@ struct ProductDetailView: View {
                     .font(.title3)
                     .bold()
                     .padding(.horizontal)
+            }
+        }
+        
+        Spacer()
+        
+        VStack(spacing: 16) {
+            Text("Quantity")
+                .font(.title3)
+                .bold()
+            
+            HStack {
+                Button {
+                    if productQuantity > 1 {
+                        productQuantity -= 1
+                    }
+                } label: {
+                    Image(systemName: "minus.circle.fill")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .bold()
+                    
+                }
+                
+                Text("\(productQuantity)")
+                    .font(.title2)
+                    .bold()
+                
+                Button {
+                    productQuantity += 1
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .bold()
+                    
+                }
             }
         }
         
