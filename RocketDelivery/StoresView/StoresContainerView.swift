d2b27a585f547018876d050ed61d26c184a2d29f
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-04-23 19:02:54 -0300

    feat: add store rating filter

--- RocketDelivery/StoresView/StoresContainerView.swift
+++ RocketDelivery/StoresView/StoresContainerView.swift
@@ -10,14 +10,40 @@ import SwiftUI
 struct StoresContainerView: View {
     
     let title = "Stores"
+    @State private var ratingFilter = 0
+    
+    var filteredStores: [StoreType] {
+        return storesMock.filter { store in
+            store.stars >= ratingFilter
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title)
-                .font(.headline)
+            
+            HStack {
+                Text(title)
+                    .font(.headline)
+                
+                Spacer()
+                
+                Menu("Filter") {
+                    ForEach(1...5, id: \.self) { rating in
+                        Button {
+                            ratingFilter = rating
+                        } label: {
+                            rating > 1 ?
+                                Text("\(rating) stars or more") :
+                                Text("\(rating) star or more")
+                        }
+
+                    }
+                }
+                .foregroundColor(.black)
+            }
             
             VStack(alignment: .leading, spacing: 30) {
-                ForEach(storesMock) { store in
+                ForEach(filteredStores) { store in
                     NavigationLink {
                         StoreDetailView(store: store)
                     } label: {
