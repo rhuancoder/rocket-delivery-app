621658f26326a8cef2992383b5301f574acd7a47
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-04-23 19:55:54 -0300

    refactor: componentizes the StoreDetailView

---
+++ RocketDelivery/StoresView/StoreDetailView/StoreDetailProductItemView.swift
@@ -0,0 +1,43 @@
+//
+//  StoreDetailProductItemView.swift
+//  RocketDelivery
+//
+//  Created by Rhuan Carvalho on 23/04/24.
+//
+
+import SwiftUI
+
+struct StoreDetailProductItemView: View {
+    
+    let product: ProductType
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            VStack(alignment: .leading, spacing: 8) {
+                Text(product.name)
+                    .bold()
+                
+                Text(product.description)
+                    .foregroundColor(.black.opacity(0.5))
+                    .multilineTextAlignment(.leading)
+                
+                Text(product.formattedPrice)
+            }
+            
+            Spacer()
+            
+            Image(product.image)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(12)
+                .frame(width: 120, height: 120)
+                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 20, x: 6, y: 8)
+        }
+        .padding()
+        .foregroundColor(.black)
+    }
+}
+
+#Preview {
+    StoreDetailProductItemView(product: storesMock[0].products[0])
+}
