f18cc0e4eb79bdf5a05ef9ddbeb6c395a57fe923
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-01-29 22:58:08 -0300

    feat: add new assets for HomeView

--- RocketDelivery/HomeView/HomeView.swift
+++ RocketDelivery/HomeView/HomeView.swift
@@ -10,31 +10,70 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var isAnimating = false
+    @State private var imageOffset: CGSize = .zero
     
     var body: some View {
-        VStack {
-            Text("Rocket Delivery")
-                .font(.system(size: 40))
-                .fontWeight(.heavy)
-                .foregroundColor(.colorRed)
-                .opacity(isAnimating ? 1 : 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Text("Order your food from the comfort of your own home.")
-                .font(.title2)
-                .padding()
-                .multilineTextAlignment(.center)
-                .foregroundColor(.black.opacity(0.7))
-                .opacity(isAnimating ? 1 : 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Spacer()
-            
-            
-        }
-        .onAppear {
-            withAnimation(.easeInOut(duration: 1.5)) {
-                isAnimating = true
+        GeometryReader { geometry in
+            ZStack {
+                
+                Circle()
+                    .foregroundStyle(.colorRed)
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(x: isAnimating ? 50 : -50, y: isAnimating ? 100 : -100)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 0)
+                
+                Circle()
+                    .foregroundStyle(.colorRedDark)
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(
+                        x: isAnimating ? geometry.size.width - 50 : geometry.size.width + 50,
+                        y: isAnimating ? geometry.size.height - 50 : geometry.size.height + 50)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 0)
+                
+                VStack {
+                    Text("Rocket Delivery")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.colorRed)
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Text("Order your food from the comfort of your own home.")
+                        .font(.title2)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black.opacity(0.7))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Image("image")
+                        .resizable()
+                        .scaledToFit()
+                        .shadow(radius: 60)
+                        .padding(isAnimating ? 32 : 92)
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(x: imageOffset.width, y: imageOffset.height)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    withAnimation(.easeInOut(duration: 0.5)) {
+                                        imageOffset = gesture.translation
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    withAnimation(.easeInOut(duration: 0.5))  {
+                                        imageOffset = .zero
+                                    }
+                                })
+                        )
+                }
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 4)) {
+                        isAnimating = true
+                    }
+                }
             }
         }
     }
