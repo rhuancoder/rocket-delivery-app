c5f9893756f29c28adf9102618554df81c786129
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-03-24 17:51:46 -0300

    feat: add confirm order

--- RocketDelivery/Networking/HomeService.swift
+++ RocketDelivery/Networking/HomeService.swift
@@ -26,14 +26,21 @@ struct HomeService {
         
         return .success(storeTypes)
     }
+    
+    func confirmOrder(product: ProductType) async throws -> Result<[String: Any]?, RequestError> {
+        guard let url = URL(string: "YOUR API URL") else {
+            return .failure(.invalidURL)
+        }
+        
+        let encodedObject = try JSONEncoder().encode(product)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = encodedObject
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let message = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+        
+        return .success(message)
+    }
 }
-
-//        URLSession.shared.dataTask(with: url) { data, _, error in
-//            if let error = error {
-//                print(error.localizedDescription)
-//            }
-//            else if let data = data {
-//                let storeTypes = try? JSONDecoder().decode([StoreType].self, from: data)
-//                print(storeTypes ?? [])
-//            }
-//        }.resume()
--- RocketDelivery/ProductView/ProductDetailView.swift
+++ RocketDelivery/ProductView/ProductDetailView.swift
@@ -11,6 +11,8 @@ struct ProductDetailView: View {
     
     let product: ProductType
     
+    var service = HomeService()
+    
     var body: some View {
         VStack {
             ProductDetailHeaderView(product: product)
@@ -21,7 +23,25 @@ struct ProductDetailView: View {
             
             Spacer()
             
-            ProductDetailButtonView()
+            ProductDetailButtonView {
+                Task {
+                    await confirmOrder()
+                }
+            }
+        }
+    }
+    
+    func confirmOrder() async {
+        do {
+            let result = try await service.confirmOrder(product: product)
+            switch result {
+            case .success(let message):
+                print(message)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        } catch {
+            print(error.localizedDescription)
         }
     }
 }
